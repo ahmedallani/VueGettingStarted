537d16690f9ce972b3cf5e11499325a9ae4fb865
ahmedallani <ahmed.allani@rbk.tn>
2020-10-06 11:45:52 +0100

    lifecycle

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,36 +1,51 @@
 <template>
   <div>
-    <div>
-    <label for="firstName">First Name:</label>
-    <input v-model="firstName" name="firstName" />
-    </div>
-    <div>
-    <label for="lastName">Last Name:</label>
-    <input v-model="lastName" />
-    </div>
-    <div id="demo">{{ fullName }}</div>
+    <button @click="changeAlive()" class="mybutton" :style="actionStyle()">
+      {{ action() }}
+    </button>
+    <Life v-if="alive" />
+    <div v-else>Destroyed</div>
   </div>
 </template>
 
 <script>
+import Life from "./Life";
 export default {
+  components: {
+    Life
+  },
   data() {
     return {
-      firstName: 'Foo5',
-      lastName: 'Bar2',
+      alive: true,
     };
   },
-  computed: {
-    fullName: function () {
-      return this.firstName + ' ' + this.lastName
+  updated() {
+    console.log(this.alive,this.action)
+  },
+  methods:{
+    changeAlive:function(){
+      console.log("changeAlive")
+      this.alive = !this.alive
+    },
+    action:function() {
+      return this.alive ? "Destroy" : "Create";
+    },
+    actionStyle:function() {
+      let color = this.alive ? "red" : "green";
+      return { backgroundColor: color };
     }
   }
 };
 </script>
 
 <style scoped>
-input,label, div{
-  margin: 5px;
+.mybutton {
+  padding: 10px;
+  color: white;
+  font-weight: bold;
+  border:none;
+  font-size: 16px;
+  border-radius: 10px;
+  margin: 10px;
 }
-
 </style>
