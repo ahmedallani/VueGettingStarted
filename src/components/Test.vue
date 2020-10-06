04d1bea35538339d5737f5a47d39b299ed48dad4
ahmedallani <ahmed.allani@rbk.tn>
2020-10-06 09:49:53 +0100

    class

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-bind:title="message">
+  <span v-bind:title="message" class="msg" :class="active ? 'active' : 'notactive'">
     Hover your mouse over me for a few seconds
     to see my dynamically bound title!
   </span>
@@ -9,12 +9,24 @@
 export default {
  data(){
      return {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        active:false
      }
  }
 }
 </script>
 
 <style>
-
+.msg{
+  color: white;
+  padding: 10px;
+  font-weight: bold;
+}
+.active{
+  background-color: aquamarine;
+  padding: 10px;
+}
+.notactive{
+  background-color: red;
+}
 </style>
